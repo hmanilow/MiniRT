11c230c7289341c8e3c61575d03a50247480f8b9
Khussein Reber <73395161+hmanilow@users.noreply.github.com>
2022-10-11 18:55:43 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,9 @@
-int main()
+#include "../minirtMAIN.h"
+
+int main(const int argc, char **argv)
 {
-	printf("Hello, World!\n");
-	return 0;
+    t_scene *scene;
+
+    scene = fd_openning(argc, argv);
+    return (0);
 }
