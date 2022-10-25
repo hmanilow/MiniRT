7fd5fd0c827e242fa13e609213c6c83587757c36
Khussein Reber <73395161+hmanilow@users.noreply.github.com>
2022-10-25 20:06:44 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,30 +1,13 @@
-#include "../minirtMAIN.h"
+#include "../inc/minirtMAIN.h"
 
-t_img * ft_init_img(t_mlx *mlx)
+int	main(int argc, char **argv)
 {
-    t_img   *img;
-    int     h;
-    int     w;
+    t_base	*base;
 
-    if (!(img = malloc(sizeof(*img))))
-        print_error("MAlloc error", MALLOC_ERROR); //malloc error macros
-    w = 1080;
-    h = 720;
-    if (!(img->img_ptr = mlx_new_image(mlx->mlx_ptr, resolution->w, resolution->h)))
-        print_error("Minilibx error", MLX_ERROR);
-    if (!(img->data = (unsigned char*)mlx_get_data_addr(img->img_ptr, &(img->bits_per_pixel), &(img->size_line), &(img->endian))))
-        print_err_and_exit("Minilibx error", MLX_ERROR);
-    return (img);
-}
-
-int main(const int argc, char **argv)
-{
-    t_scene *scene;
-    t_img   *img;
-    t_mlx   *mlx;
-
-    scene = fd_openning(argc, argv);
-    mlx = ft_malloc_mlx_init();
-    img = ft_init_img(mlx)
+    base = NULL;
+    base = init();
+    if (!parse(argc, argv, base) || !base)
+        return (1);
+    controller(base);
     return (0);
 }
