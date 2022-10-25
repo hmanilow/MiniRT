387897589224f50a0b5ab3d1fc00a66bc2fb1bbc
Khussein Reber <73395161+hmanilow@users.noreply.github.com>
2022-10-25 20:08:08 +0300

    Update utils.c

--- sources/utils/utils.c
+++ sources/utils/utils.c
@@ -1,6 +1,6 @@
-#include "../../minirtMAIN.h"
+#include "../../inc/minirtMAIN.h"
 
-static size_t	ft_strlen(const char *str)
+size_t	ft_strlen(const char *str)
 {
     unsigned int	i;
 
@@ -10,22 +10,16 @@ static size_t	ft_strlen(const char *str)
     return ((int)(i));
 }
 
-static void	ft_putstr_fd(char *s, int fd)
+void	ft_putstr_fd(char *s, int fd)
 {
     if (s == NULL)
         return ;
     write(fd, s, ft_strlen(s));
 }
 
-int ft_strncmp_reverse(char *argv, char *str, int a)
+void	arg_error(char *sms)
 {
-
-}
-
-void print_error(const char *str, const int error)
-{
-    ft_putstr_fd("Error: ", STDERR_FILENO);
-    ft_putstr_fd(str, STDERR_FILENO);
-    ft_putstr_fd("\n", STDERR_FILENO);
-    exit(error);
+    ft_putstr_fd("Error: ", 2);
+    perror(sms);
+    exit(1);
 }
